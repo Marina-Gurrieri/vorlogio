7f08ba30ca4aeae03fb697e897b2a356135ca8f7
Marina Gurrieri <gurrieri.marina@gmail.com>
2016-06-07 02:12:52 +0200

    buona notte

--- wp-content/themes/vorlogio/footer.php
+++ wp-content/themes/vorlogio/footer.php
@@ -11,7 +11,60 @@
 ?>
 
 		</section>
-		<div id="footer-container">
+		<footer id="footer">
+			<div class="nero">
+				<div class="row">
+					<?php wp_nav_menu( array( 'theme_location' => 'footer-nav' ) ); ?>
+					<ul class="reti-sociali medium-3 column">
+						<li>
+							<h4>Suivez-nous</h4>
+							<ul class="sub-menu">
+								<li>
+									<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+									<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+									<a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="pagamento medium-3 column">
+						<li>
+							<h4>Paiement sécurisé</h4>
+							<ul class="sub-menu">
+								<li>
+									<i class="fa fa-cc-paypal" aria-hidden="true"></i>
+									<i class="fa fa-cc-mastercard" aria-hidden="true"></i>
+									<i class="fa fa-cc-visa" aria-hidden="true"></i>
+								</li>
+								<li>
+									<div class="row">
+										<div class="large-6 column">
+											LIVRAISON 24h en relais <b>offerte</b>
+										</div>
+										<div class="large-6 column">
+											Livraison/Retour
+											<b>GRATUITS</b>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<div class="row newsletter">
+					<div class="small-12 column">
+						<h4>Inscription Newsletter</h4>
+						<input type="email" name="name" value="" class="email" placeholder="Saisissez votre adresse email" />
+						<input type="button" name="name" value="S'inscrire" class="buitoni" />
+					</div>
+				</div>
+			</div>
+			<div class="row bianco">
+				<div class="small-12 column">© 2016 - Vorlogio. Design et intégration réalisés par Marina Gurrieri.</div>
+			</div>
+		</footer>
+
+		<div id="footer-container hide">
 			<footer id="footer">
 				<?php do_action( 'foundationpress_before_footer' ); ?>
 				<?php dynamic_sidebar( 'footer-widgets' ); ?>
--- wp-content/themes/vorlogio/searchform.php
+++ wp-content/themes/vorlogio/searchform.php
@@ -10,6 +10,7 @@ do_action( 'foundationpress_before_searchform' ); ?>
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<?php do_action( 'foundationpress_searchform_top' ); ?>
 	<div class="input-group">
+		<i class="fa fa-search hide" aria-hidden="true"></i>
 		<input type="text" class="input-group-field" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'foundationpress' ); ?>">
 		<?php do_action( 'foundationpress_searchform_before_search_button' ); ?>
 		<div class="input-group-button">
